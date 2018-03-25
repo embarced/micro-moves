2943bfdd3e36889e8639c9c9f8552a909836c123
StefanZ <stefan.zoerner@embarc.de>
2018-03-25 22:11:30 +0200

    imports reorganized

--- modules/games/src/main/java/org/flexess/games/rest/GameRestController.java
+++ modules/games/src/main/java/org/flexess/games/rest/GameRestController.java
@@ -10,7 +10,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
