8a79ecfe8f4951c70c71172188d9c75fc3052165
StefanZ <stefan.zoerner@embarc.de>
2018-04-08 11:31:25 +0200

    Comments added.

--- modules/games/src/test/java/org/flexess/games/service/PositionTest.java
+++ modules/games/src/test/java/org/flexess/games/service/PositionTest.java
@@ -3,11 +3,21 @@ package org.flexess.games.service;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * Unit tests for Posistion class.
+ */
 public class PositionTest {
 
+    /**
+     * Creation with default constructor.
+     * Result is initial posistion, white to move.
+     */
     @Test
     public void emptyConstructor() {
+
         Position pos = new Position();
+        String fen = pos.toString();
+        Assert.assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", fen);
 
         Assert.assertEquals('w', pos.getActiveColour());
         Assert.assertEquals("KQkq", pos.getCastlingAvailability());
@@ -15,28 +25,27 @@ public class PositionTest {
         Assert.assertEquals(0, pos.getHalfmoveClock());
         Assert.assertEquals(1, pos.getFullmoveNumber());
 
-        String fen = pos.toString();
-
-        Assert.assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", fen);
     }
 
+    /**
+     * Creation with a FEN String.
+     */
     @Test
     public void fenConstructor() {
 
         String fen = "r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 1";
 
         Position pos = new Position(fen);
+        String newfen = pos.toString();
+        Assert.assertEquals(fen, newfen);
 
         Assert.assertEquals('b', pos.getActiveColour());
         Assert.assertEquals("KQkq", pos.getCastlingAvailability());
         Assert.assertEquals("-", pos.getEnPassantTargetSquare());
         Assert.assertEquals(0, pos.getHalfmoveClock());
         Assert.assertEquals(1, pos.getFullmoveNumber());
-
-        String newfen = pos.toString();
-
-        Assert.assertEquals(fen, newfen);
     }
+
     @Test
     public void getPieceBySquareName() {
         Position pos = new Position();
