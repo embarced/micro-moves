09cfe4528d6bebd969b3a27359ad8eecb129c0a0
StefanZ <stefan.zoerner@embarc.de>
2018-04-30 13:24:21 +0200

    Comments added, refactoring of variables

--- modules/chess-diagrams/test_draw.py
+++ modules/chess-diagrams/test_draw.py
@@ -1,11 +1,22 @@
 import draw
 
+
+# create an image and check dimension and color
 def test_create_image():
-    img = draw.create_image(100, 100, color='green')
-    assert img.width == 100
+    img = draw.create_image(200, 100, color='red')
+
+    assert img.width == 200
     assert img.height == 100
+
+    color_list = img.getcolors()
+    assert len(color_list) == 1
+
+    first_color = color_list[0]
+    assert first_color[0] == 200 * 100 # count
+    assert first_color[1] == (255, 0, 0, 255) # pixel (red)
 
 
 def test_draw_diagram_for_fen():
-    img = draw.draw_diagram_for_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    initial_pos_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    img = draw.draw_diagram_for_fen(initial_pos_fen)
     assert not img == None
