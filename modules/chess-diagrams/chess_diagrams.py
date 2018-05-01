7a655d69335bc075890a8fe65c06d4dd88ed3ad1
StefanZ <stefan.zoerner@embarc.de>
2018-05-01 17:25:17 +0200

    Result diagram for missing request parameter changed.

--- modules/chess-diagrams/chess_diagrams.py
+++ modules/chess-diagrams/chess_diagrams.py
@@ -19,7 +19,7 @@ def board_png():
 
     fen = flask.request.args.get("fen")
     if fen == '' or fen is None:
-        fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+        fen = '8/8/8/8/8/8/8/8 w KQkq - 0 1'
 
     image = draw.draw_diagram_for_fen(fen)
 
