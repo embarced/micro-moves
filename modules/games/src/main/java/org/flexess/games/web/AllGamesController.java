456f3619d8351b81978ca124acb700523576fe18
StefanZ <stefan.zoerner@embarc.de>
2018-05-13 15:27:29 +0200

    Home Controller added.

---
+++ modules/games/src/main/java/org/flexess/games/web/AllGamesController.java
@@ -0,0 +1,32 @@
+package org.flexess.games.web;
+
+
+import org.flexess.games.domain.Game;
+import org.flexess.games.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class AllGamesController {
+
+    @Autowired
+    GameService gameService;
+
+    @RequestMapping(value = "/games", method = RequestMethod.GET)
+    public String home(ModelMap model) {
+
+        List<Game> games = new ArrayList<>();
+        Iterable<Game> allGames = gameService.getAllGames();
+        allGames.forEach(games::add);
+
+        model.addAttribute("games", allGames);
+
+        return "all_games";
+    }
+}
