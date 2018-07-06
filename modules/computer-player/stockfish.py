66375ffa0f86c4f37d8701626decbeaae0c72df1
StefanZ <stefan.zoerner@embarc.de>
2018-07-06 18:21:53 +0200

    First version of Computer Player

---
+++ modules/computer-player/stockfish.py
@@ -0,0 +1,29 @@
+import subprocess
+import logging
+
+def calculateMove(fen, stockfish_executable ='stockfish'):
+    log = logging.getLogger(__name__)
+
+    p = subprocess.Popen([stockfish_executable],
+                        shell=True,
+                        stdin=subprocess.PIPE,
+                        stdout=subprocess.PIPE,
+                        )
+
+    cmd = 'position fen ' + fen + '\n'
+    p.stdin.write(cmd.encode())
+    p.stdin.write('go\n'.encode())
+    p.stdin.flush()
+    best_move = ""
+    while best_move == "":
+        line = p.stdout.readline()
+        line = str(line, encoding='utf-8')
+        log.debug(line)
+        if line.startswith("bestmove"):
+            best_move = line.split()[1]
+
+    p.stdin.write('quit\n'.encode())
+    p.stdin.flush()
+
+    return best_move
+
---
+++ modules/computer-player/stockfish_listener.py
@@ -0,0 +1,68 @@
+import pika
+import json
+import time
+import logging.config
+
+import stockfish
+
+STOCKFISH_EXECUTABLE = '/usr/games/stockfish'
+RABBITMQ_HOSTNAME = 'messaging'
+
+# load the logging configuration
+logging.config.fileConfig('logging.ini')
+log = logging.getLogger(__name__)
+
+def send_best_move(move):
+    channel.basic_publish(exchange='',
+                          routing_key='moves',
+                          body=move,
+                          properties=pika.BasicProperties(
+                              content_type='text/plain',
+                          )
+                          )
+
+def callback(ch, method, properties, body):
+
+    log.debug("Received %r" % body)
+    message = json.loads(body)
+
+    fen = message['fen']
+    gameId = message['gameId']
+
+    best_move = stockfish.calculateMove(fen, STOCKFISH_EXECUTABLE)
+    log.debug("Best move calculated by stockfish %r" % best_move)
+
+    response = {'move': best_move, 'gameId': gameId}
+    response_json = json.dumps(response)
+
+    send_best_move(response_json)
+
+
+
+credentials = pika.PlainCredentials('rabbitmq', 'rabbitmq')
+channel = None
+
+while True:
+    connectSucceeded = False
+    try:
+        log.info("Connect to %r" % RABBITMQ_HOSTNAME)
+        connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOSTNAME, credentials=credentials))
+        channel = connection.channel()
+        connectSucceeded = True
+    except Exception as e:
+        log.warning(e)
+        time.sleep(5)
+        pass
+    if connectSucceeded:
+        log.info("Connected.")
+        break
+
+channel.queue_declare(queue='positions')
+channel.queue_declare(queue='moves')
+
+channel.basic_consume(callback,
+                      queue='positions',
+                      no_ack=True)
+
+log.info('Stockfish waiting for messages ...')
+channel.start_consuming()
