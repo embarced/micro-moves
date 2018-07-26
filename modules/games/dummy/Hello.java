5f2c86ce537e79cead9ccd872fcb76811f74118b
StefanZ <stefan.zoerner@embarc.de>
2018-07-26 19:45:54 +0200

    Dummy classes for dependency download enforcement in Docker build

---
+++ modules/games/dummy/Hello.java
@@ -0,0 +1,7 @@
+package hello;
+
+/**
+ * Simple class to enforce compilation in Gradle within Docker without sources.
+ */
+class Hello {
+}
---
+++ modules/games/dummy/HelloTest.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.junit.Test;
+
+/**
+ * Simple test class to enforce dependency download in Gradle within Docker without "real" sources.
+ */
+public class HelloTest {
+    @Test
+    public void test() {
+    }
+}
