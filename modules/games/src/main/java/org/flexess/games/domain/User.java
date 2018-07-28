13350bc5c995a0ca7a157465a3a166ae25417aa0
StefanZ <stefan.zoerner@embarc.de>
2018-07-28 14:35:00 +0200

    User class added

---
+++ modules/games/src/main/java/org/flexess/games/domain/User.java
@@ -0,0 +1,32 @@
+package org.flexess.games.domain;
+
+/**
+ * User object used for authenticated player info.
+ */
+public class User {
+
+    private String userid;
+
+    private String name;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (userid: " + userid + ")";
+    }
+}
