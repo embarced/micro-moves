c288731c6a07d9de6753a352b4a0c23d4810a579
StefanZ <stefan.zoerner@embarc.de>
2019-03-16 13:33:31 +0100

    Comments added.

--- modules/players/web_token.py
+++ modules/players/web_token.py
@@ -2,21 +2,31 @@ import jwt
 import flask
 import os
 
+
 JWT_COOKIE_NAME = os.environ.get('JWT_COOKIE_NAME')
 JWT_SECRET = os.environ.get('JWT_SECRET')
 
 
 def user_to_jwt(user):
+    """ Converts a user object to a token.
+    The secret is used from an environment variable. """
+
     assert JWT_SECRET is not None
     return user_and_secret_to_jwt(user, JWT_SECRET)
 
 
 def jwt_to_user(token):
+    """ Converts a JWT token to a user object.
+    The secret is used from an environment variable. """
+
     assert JWT_SECRET is not None
     return jwt_and_secret_to_user(token, JWT_SECRET)
 
 
 def jwt_cookie_to_user():
+    """ Converts a JWT token from a cookie value to a user object.
+    The secret and the cookie name are used from an environment variable. """
+
     flexess_jwt = flask.request.cookies.get(JWT_COOKIE_NAME)
     if flexess_jwt is None:
         return None
@@ -25,12 +35,16 @@ def jwt_cookie_to_user():
 
 
 def user_and_secret_to_jwt(user, secret):
+    """ Converts a user object and a secret to a token. """
+
     jwt_payload = {'sub': user['userid'], 'name': user['name'] }
     encoded = jwt.encode(jwt_payload, secret, algorithm='HS256')
     return encoded
 
 
 def jwt_and_secret_to_user(token, secret):
+    """ Converts a JWT token and a secret to a user object. """
+
     decoded = jwt.decode(token, secret, algorithms=['HS256']);
     user = {}
     user['userid'] = decoded['sub']
