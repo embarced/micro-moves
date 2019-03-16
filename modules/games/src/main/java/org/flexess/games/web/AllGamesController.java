cb41cb0695377297503e06f04f3deeee87cbaac6
StefanZ <stefan.zoerner@embarc.de>
2019-03-16 11:33:33 +0100

    imports optimized

--- modules/games/src/main/java/org/flexess/games/web/AllGamesController.java
+++ modules/games/src/main/java/org/flexess/games/web/AllGamesController.java
@@ -9,9 +9,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class AllGamesController {
 
