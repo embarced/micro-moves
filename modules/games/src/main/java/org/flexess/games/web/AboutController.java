ca006944069832a3f7cd838ff8a2ed2633b104ce
StefanZ <stefan.zoerner@embarc.de>
2019-03-24 19:32:49 +0100

    About Controller Added

---
+++ modules/games/src/main/java/org/flexess/games/web/AboutController.java
@@ -0,0 +1,35 @@
+package org.flexess.games.web;
+
+
+import org.flexess.games.domain.Game;
+import org.flexess.games.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AboutController {
+
+    private GameService gameService;
+
+    public AboutController(@Autowired GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    /**
+     * Lists all games. Acts as the homepage.
+     *
+     * @param model  Spring Web MVC model for template
+     * @return template name
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(ModelMap model) {
+
+        Iterable<Game> allGames = gameService.getAllGames();
+        model.addAttribute("games", allGames);
+
+        return "all_games";
+    }
+}
