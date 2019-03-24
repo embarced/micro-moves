285b9343923693083800f90b7034a7640189a1cb
StefanZ <stefan.zoerner@embarc.de>
2019-03-24 19:33:18 +0100

    All games als start page of the Module

--- modules/games/src/main/java/org/flexess/games/web/AllGamesController.java
+++ modules/games/src/main/java/org/flexess/games/web/AllGamesController.java
@@ -1,9 +1,6 @@
 package org.flexess.games.web;
 
 
-import org.flexess.games.domain.Game;
-import org.flexess.games.service.GameService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,24 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AllGamesController {
 
-    private GameService gameService;
-
-    public AllGamesController(@Autowired GameService gameService) {
-        this.gameService = gameService;
-    }
-
     /**
-     * Lists all games. Acts as the homepage.
+     * About this module.
      *
-     * @param model  Spring Web MVC model for template
      * @return template name
      */
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(ModelMap model) {
-
-        Iterable<Game> allGames = gameService.getAllGames();
-        model.addAttribute("games", allGames);
-
-        return "all_games";
+    @RequestMapping(value = "/about", method = RequestMethod.GET)
+    public String about(ModelMap model) {
+        return "about";
     }
 }
