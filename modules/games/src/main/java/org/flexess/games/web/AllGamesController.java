3a4f39cfdd8a3adc9338fba43e7a60fe61b0031f
StefanZ <stefan.zoerner@embarc.de>
2019-04-10 20:06:28 +0200

    Fixed wrong class names

--- modules/games/src/main/java/org/flexess/games/web/AllGamesController.java
+++ modules/games/src/main/java/org/flexess/games/web/AllGamesController.java
@@ -1,6 +1,9 @@
 package org.flexess.games.web;
 
 
+import org.flexess.games.domain.Game;
+import org.flexess.games.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +12,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AllGamesController {
 
+    private GameService gameService;
+
+    public AllGamesController(@Autowired GameService gameService) {
+        this.gameService = gameService;
+    }
+
     /**
-     * About this module.
+     * Lists all games. Acts as the homepage.
      *
+     * @param model  Spring Web MVC model for template
      * @return template name
      */
-    @RequestMapping(value = "/about", method = RequestMethod.GET)
-    public String about(ModelMap model) {
-        return "about";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String allGames(ModelMap model) {
+
+        Iterable<Game> allGames = gameService.getAllGames();
+        model.addAttribute("games", allGames);
+
+        return "all_games";
     }
 }
