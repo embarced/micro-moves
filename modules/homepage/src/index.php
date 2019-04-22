090ae5cf886ca01828f05877e7042a1d11cf0c3d
StefanZ <stefan.zoerner@embarc.de>
2019-04-22 09:14:44 +0200

    Icons for the Button-Links for modules

--- modules/homepage/src/index.php
+++ modules/homepage/src/index.php
@@ -16,11 +16,19 @@
                 See <a href="https://www.embarc.de/micro-moves/" target="_blank">Micro Moves</a> blog series for details.
             </p>
             <p>
-                <a class = "btn btn-default btn-lg" role = "button" href="/games/">Games</a>
-                <a class = "btn btn-default btn-lg" role = "button" href="/players/">Players</a>
-                <a class = "btn btn-default btn-lg" role = "button" href="/players/register">Register</a>
+                <a class = "btn btn-default btn-lg" role = "button" href="/games/"><span class="glyphicon glyphicon-th" aria-hidden="true"></span> Games</a>
+                <a class = "btn btn-default btn-lg" role = "button" href="/players/"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Players</a>
             </p>
         </div>
+
+        <h2>Resources</h2>
+        <ul>
+            <li><a href="https://spring.io/projects/spring-boot" target="_blank">Spring Boot</a> - makes it easy to create stand-alone, production-grade Spring based Applications</li>
+            <li><a href="https://www.baeldung.com/jackson-annotations" target="_blank">Jackson Annotation Examples</a> - a deep dive into Jackson Annotations by Eugen Paraschiv</li>
+            <li><a href="https://www.thymeleaf.org" target="_blank">Thymeleaf</a> - a modern server-side Java template engine</li>
+            <li><a href="https://spring.io/guides/gs/messaging-stomp-websocket/" target="_blank">WebSocket guide</a> - Using WebSocket to build an interactive web application</li>
+        </ul>
+
 
     </div>
 
